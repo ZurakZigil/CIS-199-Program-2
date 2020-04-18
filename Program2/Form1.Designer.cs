d57095c3b87c5c99e6da463ff42a3ef25f092290
Christopher Abbott <chris.abbott2757@gmail.com>
2020-04-17 22:21:47 -0400

    Initial commit

---
+++ Program2/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Program2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tBoxHours = new System.Windows.Forms.TextBox();
+            this.labelHours = new System.Windows.Forms.Label();
+            this.displayYear = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.tBoxLastName = new System.Windows.Forms.TextBox();
+            this.labelComeOnWhyYouEvenCheckingThisBetterNotBe = new System.Windows.Forms.Label();
+            this.displayRegTime = new System.Windows.Forms.Label();
+            this.buttEnter = new System.Windows.Forms.Button();
+            this.cBoxGraduate = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.displayRegDate = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tBoxHours
+            // 
+            this.tBoxHours.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tBoxHours.Location = new System.Drawing.Point(337, 67);
+            this.tBoxHours.Name = "tBoxHours";
+            this.tBoxHours.Size = new System.Drawing.Size(149, 33);
+            this.tBoxHours.TabIndex = 0;
+            // 
+            // labelHours
+            // 
+            this.labelHours.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHours.Location = new System.Drawing.Point(197, 72);
+            this.labelHours.Name = "labelHours";
+            this.labelHours.Size = new System.Drawing.Size(123, 26);
+            this.labelHours.TabIndex = 1;
+            this.labelHours.Text = "Hours: ";
+            // 
+            // displayYear
+            // 
+            this.displayYear.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayYear.Location = new System.Drawing.Point(197, 298);
+            this.displayYear.Name = "displayYear";
+            this.displayYear.Size = new System.Drawing.Size(183, 34);
+            this.displayYear.TabIndex = 2;
+            this.displayYear.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLastName.Location = new System.Drawing.Point(197, 196);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(134, 26);
+            this.labelLastName.TabIndex = 4;
+            this.labelLastName.Text = "Last Name:";
+            // 
+            // tBoxLastName
+            // 
+            this.tBoxLastName.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tBoxLastName.Location = new System.Drawing.Point(337, 193);
+            this.tBoxLastName.Name = "tBoxLastName";
+            this.tBoxLastName.Size = new System.Drawing.Size(320, 33);
+            this.tBoxLastName.TabIndex = 3;
+            // 
+            // labelComeOnWhyYouEvenCheckingThisBetterNotBe
+            // 
+            this.labelComeOnWhyYouEvenCheckingThisBetterNotBe.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelComeOnWhyYouEvenCheckingThisBetterNotBe.Location = new System.Drawing.Point(492, 70);
+            this.labelComeOnWhyYouEvenCheckingThisBetterNotBe.Name = "labelComeOnWhyYouEvenCheckingThisBetterNotBe";
+            this.labelComeOnWhyYouEvenCheckingThisBetterNotBe.Size = new System.Drawing.Size(178, 30);
+            this.labelComeOnWhyYouEvenCheckingThisBetterNotBe.TabIndex = 5;
+            this.labelComeOnWhyYouEvenCheckingThisBetterNotBe.Text = "(Without Spring 2018)";
+            this.labelComeOnWhyYouEvenCheckingThisBetterNotBe.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // displayRegTime
+            // 
+            this.displayRegTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.displayRegTime.Font = new System.Drawing.Font("Arial Black", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayRegTime.Location = new System.Drawing.Point(197, 405);
+            this.displayRegTime.Name = "displayRegTime";
+            this.displayRegTime.Size = new System.Drawing.Size(460, 73);
+            this.displayRegTime.TabIndex = 6;
+            this.displayRegTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttEnter
+            // 
+            this.buttEnter.Location = new System.Drawing.Point(496, 242);
+            this.buttEnter.Name = "buttEnter";
+            this.buttEnter.Size = new System.Drawing.Size(161, 37);
+            this.buttEnter.TabIndex = 7;
+            this.buttEnter.Text = "Enter";
+            this.buttEnter.UseVisualStyleBackColor = true;
+            this.buttEnter.Click += new System.EventHandler(this.buttEnter_Click);
+            // 
+            // cBoxGraduate
+            // 
+            this.cBoxGraduate.AutoSize = true;
+            this.cBoxGraduate.Location = new System.Drawing.Point(337, 106);
+            this.cBoxGraduate.Name = "cBoxGraduate";
+            this.cBoxGraduate.Size = new System.Drawing.Size(103, 24);
+            this.cBoxGraduate.TabIndex = 8;
+            this.cBoxGraduate.Text = "Graduate";
+            this.cBoxGraduate.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-9, -10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(171, 551);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // displayRegDate
+            // 
+            this.displayRegDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.displayRegDate.Font = new System.Drawing.Font("Arial Black", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayRegDate.Location = new System.Drawing.Point(197, 332);
+            this.displayRegDate.Name = "displayRegDate";
+            this.displayRegDate.Size = new System.Drawing.Size(460, 73);
+            this.displayRegDate.TabIndex = 10;
+            this.displayRegDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.buttEnter;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 520);
+            this.Controls.Add(this.displayRegDate);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cBoxGraduate);
+            this.Controls.Add(this.buttEnter);
+            this.Controls.Add(this.displayRegTime);
+            this.Controls.Add(this.labelComeOnWhyYouEvenCheckingThisBetterNotBe);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.tBoxLastName);
+            this.Controls.Add(this.displayYear);
+            this.Controls.Add(this.labelHours);
+            this.Controls.Add(this.tBoxHours);
+            this.Name = "Form1";
+            this.Text = "Registration Time";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tBoxHours;
+        private System.Windows.Forms.Label labelHours;
+        private System.Windows.Forms.Label displayYear;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.TextBox tBoxLastName;
+        private System.Windows.Forms.Label labelComeOnWhyYouEvenCheckingThisBetterNotBe;
+        private System.Windows.Forms.Label displayRegTime;
+        private System.Windows.Forms.Button buttEnter;
+        private System.Windows.Forms.CheckBox cBoxGraduate;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label displayRegDate;
+    }
+}
+
---
+++ Program2/Form1.cs
@@ -0,0 +1,132 @@
+﻿/*
+Grading ID: W9957
+Program#: 2
+Due Date: 3/08/2018 11:59.00
+Course Section: 199-01
+
+Description:
+A program to determine when a student attending UofL can register for classes. 
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttEnter_Click(object sender, EventArgs e)
+        {
+            string lastName; //holds the user's last name
+            const byte i = 0; //constant for 0 when finding the first letter of the user's name
+
+            //dates and time for graduate students
+            const string GRD_DATE = "MARCH 27";
+            const string GRD_AZ = "10:30 AM";
+
+            //Hours requirements of classes
+            const int intSenior = 90;       //senior
+            const int intJunior = 60;       //junior
+            const int intSophomore = 30;    //sophomore
+
+            //Dates for seperate classes (1 & 2 to designate the first and second days chronologically)
+            const string SNR_DATE = "MARCH 28";     //senior
+            const string JNR_DATE = "MARCH 29";     //junior
+            const string SOP_DATE1 = "MARCH 30";    //sophomore day 1
+            const string SOP_DATE2 = "APRIL 2";     //sophomore day 2
+            const string FRS_DATE1 = "APRIL 3";     //freshman day 1
+            const string FRS_DATE2 = "APRIL 4";     //freshman day 1
+
+            //constants for times
+            const string TIME_1 = "8:30 AM";
+            const string TIME_2 = "10:00 AM";
+            const string TIME_3 = "11:30 AM";
+            const string TIME_4 = "2:00 PM";
+            const string TIME_5 = "4:00 PM";
+
+            //used to see whether the user is a graduate, avoids all other logic
+            if (cBoxGraduate.Checked)
+            {
+                displayRegDate.Text = GRD_DATE;
+                displayRegTime.Text = GRD_AZ;
+                displayYear.Text = "Graduate";
+            }
+            else
+            {
+                if ((!double.TryParse(tBoxHours.Text, out double hours)) || hours < 0) //checks that the value entered is a non-negative double
+                {
+                    MessageBox.Show("Please Enter Valid Number");
+                }
+                else if (string.IsNullOrWhiteSpace(tBoxLastName.Text) || !char.IsLetter(tBoxLastName.Text[i])) //checks whether a value was typed and whether it was a letter 
+                {
+                    MessageBox.Show("Please Enter Valid Name");
+                }
+                else
+                {
+                    lastName = tBoxLastName.Text;               //declares the users last name from the text box
+                    char firstInitial = lastName.ToUpper()[i];  //determines the first letter of their last name
+
+                    //tests for junior and senior
+                    if (hours >= intJunior)
+                    {
+                        if (hours >= intSenior) // testing whether they are a senior
+                        {
+                            displayYear.Text = $"Senior: {firstInitial}";
+                            displayRegDate.Text = SNR_DATE; //displays senior date
+                        }
+                        else //or ELSE they are a junior
+                        {
+                            displayYear.Text = $"Junior: {firstInitial}";
+                            displayRegDate.Text = JNR_DATE;  //displays junior date
+                        }
+
+                        //determines the time based on char
+                        if (firstInitial <= 'D') { displayRegTime.Text = TIME_1; }
+                        else if (firstInitial <= 'I') { displayRegTime.Text = TIME_2; }
+                        else if (firstInitial <= 'O') { displayRegTime.Text = TIME_3; }
+                        else if (firstInitial <= 'S') { displayRegTime.Text = TIME_4; }
+                        else if (firstInitial <= 'Z') { displayRegTime.Text = TIME_5; }
+                    }
+                    //tests for freshman and sophomores
+                    else
+                    {
+                        if (hours >= intSophomore) //testing whether they are a sophomore 
+                        {
+                            displayYear.Text = $"Sophomore: {firstInitial}";
+                            //tests for the seperating leter of L for the days
+                            if (firstInitial <= 'L') { displayRegDate.Text = SOP_DATE1; } 
+                            else { displayRegDate.Text = SOP_DATE2; }
+                        }
+                        else //or ELSE they are a freshman
+                        {
+                            displayYear.Text = $"Freshman: {firstInitial}";
+                            //tests for the seperating leter of L for the days
+                            if (firstInitial <= 'L') { displayRegDate.Text = FRS_DATE1; }
+                            else { displayRegDate.Text = FRS_DATE2; }
+                        }
+
+                        //determines the time based on char. Holds other tests because of the ranges of repeating times but for different letter ranges
+                        if (firstInitial <= 'B' || (firstInitial >= 'M' && firstInitial <='O')) { displayRegTime.Text = TIME_1; }
+                        else if (firstInitial <= 'D' || (firstInitial >= 'P' && firstInitial <= 'Q')) { displayRegTime.Text = TIME_2; }
+                        else if (firstInitial <= 'F' || (firstInitial >= 'R' && firstInitial <= 'S')) { displayRegTime.Text = TIME_3; }
+                        else if (firstInitial <= 'I' || (firstInitial >= 'T' && firstInitial <= 'V')) { displayRegTime.Text = TIME_4; }
+                        else if (firstInitial <= 'L' || firstInitial >= 'W' ) { displayRegTime.Text = TIME_5; }
+                    }
+                }
+            }
+        }
+    }
+}
